06a2d9686d7b4102a852f4dba75068b6fbedad59
Mr. T <austinbook@Christophers-MacBook-Pro.local>
2019-07-08 13:00:26 -0600

    Created files, mmade basic html and initial pseudocoded javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+//Have a randomly generated number display to the user. The user clicks on one of the crystals which has an assigned randomly generated number.
+//When the user clicks on the crystal is adds to a total value that is displayed. The user wants to add numbers to match the initial generated number
+//When the user reaches that value wins are increased and all new numbers are generated
+//When the user goes over the value losses are increased and all new numbers are generated
+//
+//
+//
+//
+//Variables
+//
+//
+//Functions
+//
+//
+//Event listeners
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
